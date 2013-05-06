378fc6b84f234c2415c49a9952465ce9ba4f171d
Paul Mougel <mougelpaul@gmail.com>
2013-05-06 19:23:09 +0200

    Added basic document editing functionnality to the edit view

--- src/front/app/scripts/controllers/edit.js
+++ src/front/app/scripts/controllers/edit.js
@@ -4,4 +4,20 @@ angular.module('pie')
 .controller('EditController', function ($scope, $resource, $routeParams) {
 	var Document = $resource('/api/document/:id', {id: '@id'});
 	$scope.document = Document.get($routeParams.documentId);
+
+	$scope.isMyContentEditable = false;
+
+	$scope.edit = function() {
+		if (! $scope.isMyContentEditable) {
+			$scope.isMyContentEditable = true;
+		}
+		else {
+			$scope.isMyContentEditable = false;
+			$scope.document.$save();
+		}
+	};
+
+	$scope.editButtonText = function() {
+		return $scope.isMyContentEditable ? "Save" : "Edit";
+	};
 });
