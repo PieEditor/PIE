1deca0881e10c4f54af51e33051811361924d176
Guillaume Burel <guillaume@yimgo.fr>
2013-05-13 10:26:58 +0200

    Handle CORS's OPTIONS request.
    HOTFIX - https://gist.github.com/nilcolor/816580.

--- src/back/server.js
+++ src/back/server.js
@@ -15,10 +15,29 @@ server.on('request', function(request, response) {
 	request.on('end', function() {
 		var params = JSON.parse(body);
 
+		/* CORS handling
+		 * Thanks to nilcolor.
+		 * https://gist.github.com/nilcolor/816580 
+		 */
+
+		if (request.method === 'OPTIONS') {
+			console.log('!OPTIONS');
+			var headers = {};
+			// IE8 does not allow domains to be specified, just the *
+			// headers["Access-Control-Allow-Origin"] = req.headers.origin;
+			headers["Access-Control-Allow-Origin"] = "*";
+			headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+			headers["Access-Control-Allow-Credentials"] = false;
+			headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+			headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+			response.writeHead(200, headers);
+			response.end();
+		}
+
 		/* USER */
 
 		// Sign in
-		if (request.url == "/users/signin" && request.method == "POST") {
+		else if (request.url == "/users/signin" && request.method == "POST") {
 			var shasum = crypto.createHash('sha512').update(params.pass, 'utf8').digest('hex');
 			couchWrapper.userLogin(params.user, function(user_data) {
 				if (shasum == user_data.hash) {
