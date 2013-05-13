cad64d26061fc13769bfb0a44a760dee8e39a9f1
Paul Mougel <mougelpaul@gmail.com>
2013-05-13 14:58:51 +0200

    authService now stores the auth token in a cookie

--- src/front/app/scripts/services/authService.js
+++ src/front/app/scripts/services/authService.js
@@ -6,19 +6,18 @@
 	- on a change, it should update its $scope variable accordingly
 */
 angular.module('pie')
-.factory('authService', function($http) {
+.factory('authService', function($http, $cookieStore) {
 	return {
-		token: undefined,
 		authenticated: false,
 		login: function(login, passwd, successCallback, errorCallback) {
 			var t = this;
-			t.authenticated = false;
-			t.token = undefined;
+			$cookieStore.put('authenticated', false);
+			$cookieStore.put('token', undefined);
 
 			$http.post('http://localhost:8080/users/signin', {login:login, passwd:passwd})
 			.success(function(data) {
-				t.authenticated = true;
-				t.token = data.token;
+				$cookieStore.put('authenticated', true);
+				$cookieStore.put('token', data.token);
 				successCallback();
 			})
 			.error(function(data) {
@@ -27,14 +26,14 @@ angular.module('pie')
 		},
 		register: function(login, passwd, email, imgUrl, successCallback, errorCallback) {
 			var t = this;
-			t.authenticated = false;
-			t.token = undefined;
+			$cookieStore.put('authenticated', false);
+			$cookieStore.put('token', undefined);
 
 			var u = {login:login, passwd:passwd, email:email, imgUrl:imgUrl};
 			$http.post('http://localhost:8080/users/signup', {user: u})
 			.success(function(data) {
-				t.authenticated = true;
-				t.token = data.token;
+				$cookieStore.put('authenticated', true);
+				$cookieStore.put('token', data.token);
 				successCallback();
 			})
 			.error(function(data) {
