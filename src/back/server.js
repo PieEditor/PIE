44fd82d7f35f75d0a68cab119e08b37fbebb3bc4
Guillaume Burel <guillaume@yimgo.fr>
2013-05-13 08:45:29 +0200

    Update backend server.
    Handle more API calls, fix wrapper call because of Fabio's renaming.

--- src/back/server.js
+++ src/back/server.js
@@ -1,4 +1,4 @@
-var couchWrapper = require('./couch_wrapper')
+var couchWrapper = require('./couch-wrapper')
 
 var http = require('http');
 var crypto = require('crypto');
@@ -21,7 +21,7 @@ server.on('request', function(request, response) {
 		if (request.url == "/users/signin" && request.method == "POST") {
 			var shasum = crypto.createHash('sha512').update(params.pass, 'utf8').digest('hex');
 			couchWrapper.userLogin(params.user, function(user_data) {
-				if (shasum == user_data.shasum) {
+				if (shasum == user_data.hash) {
 					users[user_data.uuid] = params.user;
 					response.writeHead(200, "OK");
 					response.write(JSON.stringify({token: user_data.uuid}));
@@ -37,7 +37,7 @@ server.on('request', function(request, response) {
 		else if (request.url == "/user/signout" && request.method == "POST") {
 			if (users[params.token]) {
 				delete users[params.token];
-				response.writeHead(205, "Reset Content");				
+				response.writeHead(204, "No Content");				
 			}
 			else {
 				response.writeHead(401, "Unauthorized");
@@ -52,7 +52,7 @@ server.on('request', function(request, response) {
 			params.user.shasum = shasum;
 			// ... then delete the password
 			delete params.user.pass;
-			/*couchWrapper.userAdd(params.user, function(uuid) {
+			couchWrapper.userCreate(params.user, function(uuid) {
 				if (uuid) {
 					users[uuid] = params.user.username
 					response.writeHead(201, "Created");
@@ -62,13 +62,13 @@ server.on('request', function(request, response) {
 					response.writeHead(403, "Forbidden");
 				}
 				response.end();
-			});*/
+			});
 		}
 
-		// Get a signle user or get the authenticated user
-		else if ((request.url.indexOf('/users'/) == 0 || request.url == "/user") && request.method == "GET") {
+		// Get a single user or get the authenticated user
+		else if ((request.url.indexOf('/users/') == 0 || request.url == "/user") && request.method == "GET") {
 			// determine the user
-			var user = request.url == "/user" ? users[params.token] : request.url.substr('/users/'.length - 1);
+			var user = request.url == "/user" ? users[params.token] : request.url.substr('/users/'.length);
 			if (users[params.token]) {
 				/*couchWrapper.userGet(user, function(user_object) {
 					if (user_object) {
@@ -90,15 +90,15 @@ server.on('request', function(request, response) {
 		// Delete the authenticated user
 		else if (request.url == "/user" && request.method == "DELETE") {
 			if (users[params.token]) {
-				/*couchWrapper.userDelete(users[params.token], function(success) {
+				couchWrapper.userDelete(users[params.token], function(success) {
 					if (success) {
-						response.writeHead(205, "Reset Content");
+						response.writeHead(204, "No Content");
 					}
 					else {
 						response.writeHead(403, "Forbidden");
 					}
 					response.end();
-				});*/
+				});
 				delete users[params.token];		
 			}
 			else {
@@ -111,32 +111,105 @@ server.on('request', function(request, response) {
 
 		// Create a single document
 		else if (request.url == "/documents" && request.method == "POST") {
-			// TODO
+			if (users[params.token]) {
+				couchWrapper.docAdd(params.document, function(id) {
+					if (id) {
+						response.writeHead(201, "Created");
+						response.write(JSON.stringify({id: id}));
+					}
+					else {
+						response.writeHead(403, "Forbidden");
+					}
+					response.end();
+				});
+			}
+			else {
+				response.writeHead(401, "Unauthorized");
+				response.end();
+			}
 		}
 
 		// Update a document
 		else if (request.url.indexOf("/documents/") == 0 && request.method == "PUT") {
-			// TODO
+			//var doc_id = parseInt(request.url.substr('/documents/'.length));
+			if (users[params.token]) {
+				couchWrapper.docUpdate(params.document, function(success) {
+					if (success) {
+						response.writeHead(204, "No Content");
+					}
+					else {
+						response.writeHead(403, "Forbidden");
+					}
+					response.end();
+				});
+			}
+			else {
+				response.writeHead(401, "Unauthorized");
+				response.end();
+			}
+
 		}
 
 		// Delete a document
 		else if (request.url.indexOf("/documents/") == 0 && request.method == "DELETE") {
-			// TODO
+			var doc_id = parseInt(request.url.substr('/documents/'.length));
+			if (users[params.token]) {
+				couchWrapper.docDelete(doc_id, function(success) {
+					if (success) {
+						response.writeHead(204, "No Content");
+					}
+					else {
+						response.writeHead(403, "Forbidden");
+					}
+					response.end();
+				});
+			}
+			else {
+				response.writeHead(401, "Unauthorized");
+				response.end();
+			}
 		}
 
-		// List your documents
-		else if (request.url == "/documents" && request.method == "GET") {
-			// TODO
-		}
-
-		// List user documents
-		else if (request.url.indexOf("/users/") == 0 && request.url.indexOf("/documents") == request.url.length - "/documents".length && request.method == "GET") {
-			// TODO
+		// List your documents or user documents
+		else if ((request.url == "/documents" || request.url.indexOf("/users/") == 0 && request.url.indexOf("/documents") == request.url.length - "/documents".length) && request.method == "GET") {
+		var user = request.url == "/documents" ? users[params.token] : request.url.substr('/users'.length, request.url.indexOf('/documents') - 1);
+			if (user) {
+				/*couchWrapper.docUserList(users[params.token], function(doc_ids) {
+					if (doc_ids) {
+						response.writeHead(200, "OK");
+						response.write(JSON.stringify({ids: doc_ids}));
+					}
+					else {
+						response.writeHead(403, "Forbidden");
+					}
+					response.end();
+				});*/
+			}
+			else {
+				response.writeHead(401, "Unauthorized");
+				response.end();
+			}
 		}
 
 		//Get a single document
 		else if (request.url.indexOf("/documents/") == 0 && request.method == "GET") {
-			// TODO
+			var doc_id = parseInt(request.url.substr('/documents/'.length));
+			if (users[params.token]) {
+				/*couchWrapper.docGet(doc_id, function(doc) {
+					if (doc) {
+						response.writeHead(200, "OK");
+						response.write(doc);
+					}
+					else {
+						response.writeHead(403, "Forbidden");
+					}
+					response.end();
+				});*/
+			}
+			else {
+				response.writeHead(401, "Unauthorized");
+				response.end();
+			}
 		}
 
 		/* Discussion */
