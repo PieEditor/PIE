c6661e8733c8cd312e03977705fcad0cfcb9c389
Baptiste Metge <baptiste.metge@insa-lyon.fr>
2013-05-13 16:26:21 +0200

    Galère pour afficher le plan

--- src/front/app/scripts/controllers/createNewDoc.js
+++ src/front/app/scripts/controllers/createNewDoc.js
@@ -4,15 +4,17 @@ angular.module('pie')
 .controller('CreateNewDocController', function ($scope, $resource, $routeParams,$location,$timeout) {
 	
 	$scope.architectureLevels = [
-		{text:'First part title...', level:1, deleted:false},
-		{text:'SUbFirst part title...', level:2, deleted:false},
-		{text:'SubFirst part title...', level:2, deleted:false},
-		{text:'Second part title...', level:1, deleted:false},
-		{text:'SubSecond part title...', level:2, deleted:false},
-		{text:'SubSecond part title...', level:2, deleted:false},
-		{text:'SubSubSecond part title...', level:3, deleted:false},
-		{text:'SubSubSecond part title...', level:3, deleted:false},
-		{text:'Thrid part title...', level:1, deleted:false}
+		{text:'First section title...', level:1, deleted:false},
+		{text:'Sub First section title...', level:2, deleted:false},
+		{text:'Sub First section title...', level:2, deleted:false},
+		{text:'Sub First section title...', level:3, deleted:false},
+		{text:'Sub First section title...', level:2, deleted:false},
+		{text:'Second section title...', level:1, deleted:false},
+		{text:'Sub Second section title...', level:2, deleted:false},
+		{text:'Sub Second section title...', level:2, deleted:false},
+		{text:'Third section title...', level:1, deleted:false},
+		{text:'Sub third section title...', level:2, deleted:false},
+		{text:'Sub third section title...', level:2, deleted:false},
     ];
 	$scope.size =  _.size($scope.architectureLevels);
 	/* remove a part and all the subparts in it */
@@ -37,11 +39,44 @@ angular.module('pie')
 	$scope.getPartLevel = function(part) {
 		return part.level;
 	};
+
+	/* return the text of a part */
+	$scope.getPartIndice = function(part,architectureLevels) {
+		var i = 1;
+		var j = 0 ;
+		var indice="";
+		var myIndex = _.indexOf( architectureLevels, part)  ;
+		var myLevel =  architectureLevels[myIndex].level ;
+		for ( i=1; i<= myLevel; i++ ) {
+			var count = 0 ;
+			
+			for (j=0;j<=myIndex;j++) {
+				if (j > 0 && architectureLevels[j-1] > i) {
+					count = 0;
+				}
+				if (architectureLevels[j].level == i) {
+					count++;
+				}
+			}
+			
+			indice += count + '.';
+		}
+		return indice; 
+	};
 	
-	/* Add a sub part where in the selected part */
+	/* Add a sub part in the selected part */
 	$scope.createSubPart = function(part) {
 		var index = _.indexOf( $scope.architectureLevels, part) ;
-		$scope.architectureLevels.splice(index, 0, {});
+		var myNewLevel =  $scope.architectureLevels[index].level + 1;
+		$scope.architectureLevels.splice(index+1, 0, { text : 'New sub section title...' , level : myNewLevel , deleted : false });
+		$scope.size =  _.size($scope.architectureLevels);
+	};
+	
+	/* Add a part of the same level under the selected part */
+	$scope.createNewPart = function(part) {
+		var index = _.indexOf( $scope.architectureLevels, part) ;
+		var myNewLevel =  $scope.architectureLevels[index].level ;
+		$scope.architectureLevels.splice(index+1, 0, { text : 'New section title...' , level : myNewLevel , deleted : false });
 		$scope.size =  _.size($scope.architectureLevels);
 	};
 
