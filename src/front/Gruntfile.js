6cd7a9df8a199f36e70e54da02d48fc328e433c5
Pie Server <yimgo@yimgo.fr>
2013-05-15 10:58:37 +0200

    Remove target 'test' from task 'build'.
    In order to build on a headless server.

--- src/front/Gruntfile.js
+++ src/front/Gruntfile.js
@@ -283,7 +283,7 @@ module.exports = function (grunt) {
   grunt.registerTask('build', [
     'clean:dist',
     'jshint',
-    'test',
+    //'test',
     'coffee',
     'compass:dist',
     'useminPrepare',
