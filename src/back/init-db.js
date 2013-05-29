49352160a4d49d40ea350f733c54df6af23e4578
Izelenn <izelenn@gmail.com>
2013-05-29 11:51:34 +0200

    Updated db updates

--- src/back/init-db.js
+++ src/back/init-db.js
@@ -1,6 +1,4 @@
 var http = require("http");
-var mapDoc = require("./map-doc");
-var mapUsr = require("./map-usr");
 var db = require("./couch-wrapper");
 
 var host = "localhost", port = 5984;
@@ -8,24 +6,11 @@ var host = "localhost", port = 5984;
 var user = http.request({port: port, host: host, method: "PUT", path: "/user"},
 	function(res) {
 		console.log("User database creation : " + res.statusCode);
-		mapfuncDoc.end();
+		process.exit();
 	});
 var document = http.request({port: port, host: host, method: "PUT", path: "/document"},
 	function(res) {
 		console.log("Document database creation : " + res.statusCode);
 		user.end();
 	});
-var mapfuncDoc = http.request({port: port, host: host, method: "PUT", path: "/document/_design/application"},
-	function(res) {
-		console.log("Document map functions insertion : " + res.statusCode)
-		mapfuncUsr.end();
-	});
-var mapfuncUsr = http.request({port: port, host: host, method: "PUT", path: "/user/_design/application"},
-	function(res) {
-		console.log("User map function insertion : " + res.statusCode)
-		process.exit();
-	});
-mapfuncDoc.write(JSON.stringify(mapDoc));
-mapfuncUsr.write(JSON.stringify(mapUsr));
 document.end();
-
