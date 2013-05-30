ce039528cb2d37448ea5661a4bd4c9bfb4ef6d46
Paul Mougel <mougelpaul@gmail.com>
2013-05-30 09:58:36 +0200

    Added authorization check on discussion close

---
+++ src/front/app/scripts/services/permissionService.js
@@ -0,0 +1,23 @@
+angular.module('pie')
+.factory('permissionService', function($injector, $http, apiBaseUrl, authService) {
+	return {
+		iCanCloseCurrentDiscussion: function() {
+			// HACK: Resolve circular dependency via manual injection
+			var documentService = $injector.get('documentService');
+			var discussionService = $injector.get('discussionService');
+
+			var currentUser = authService.user.login;
+			var documentOwner = documentService.currentDocument.owner;
+			var sectionOwner;
+
+			_.map(documentService.currentDocument.content, function(section) {
+				_.map(section.discussions, function(discussion) {
+					if (discussion == discussionService.currentDiscussion)
+						sectionOwner = section.owner.login;
+				});
+			});
+
+			return (currentUser == documentOwner || currentUser == sectionOwner);
+		}
+	};
+});
