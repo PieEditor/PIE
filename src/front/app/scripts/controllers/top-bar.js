71e07136ca0c97b77a43e0fe4be3188d1797b92f
Paul Mougel <mougelpaul@gmail.com>
2013-06-02 15:45:16 +0200

    Added a top navigation bar

---
+++ src/front/app/scripts/controllers/top-bar.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('pie')
+.controller('TopBarController', function ($scope, authService) {
+	$scope.isSectionActive = function(section) {
+		return ($scope.currentPage == section) ? 'active' : '';
+	};
+
+	$scope.$watch(
+		function() { return authService.user; },
+		function(newVal) {
+			if (! authService.user) return;
+			$scope.login = authService.user.login;
+		}
+	);
+	$scope.logout = authService.logout;
+});
