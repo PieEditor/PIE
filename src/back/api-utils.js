PieEditor/PIE

commit e4155fb7e901d4e6d1ca79ce6226e7d151ce4c75

Improved server code quality.
Using JSLint.

src/back/api-utils.js
@@ -1,57 +1,62 @@
+"use strict";
+
 var http = require("http");
 
 var users = [];
-users["token"] = "kikoo";
+users.token = "kikoo";
 
-exports.registerUser = function(token, login) {
+var exports = {};
+
+exports.registerUser = function (token, login) {
 	users[token] = login;
-}
+};
 
-exports.unregisterUser = function(token) {
+exports.unregisterUser = function (token) {
 	delete users[token];
-}
+};
 
-exports.getLogin = function(token) {
+exports.getLogin = function (token) {
 	return users[token];
-}
+};
 
-function parseParams(request, body) {				
+function parseParams(request, body) {
 	var params = {};
-	if ((request.method === "POST" || request.method === "PUT" || request.method === "DELETE") && body)
+	if ((request.method === "POST" || request.method === "PUT" || request.method === "DELETE") && body) {
 		params = JSON.parse(body);
-	else if (request.method === "GET")
+	} else if (request.method === "GET") {
 		params = require("url").parse(request.url, true).query;
+	}
 	return params;
 }
 
 function getToken(headers) {
-	// parse cookies in order to get the access token - thanks to Corey Hart & ianj : http://stackoverflow.com/questions/3393854/get-and-set-a-single-cookie-with-node-js-http-server
 	var cookies = {};
-	headers.cookie && headers.cookie.split(";").forEach(function(cookie) {
-		var parts = cookie.split("=");
-		cookies[parts[0].trim()] = (parts[1] || "").trim();
-	});
-	return cookies["token"];
+	if (headers.cookie) {
+		headers.cookie.split(";").forEach(function (cookie) {
+			var parts = cookie.split("=");
+			cookies[parts[0].trim()] = (parts[1] || "").trim();
+		});
+	}
+	return cookies.token;
 }
 
 var calls = [];
-exports.register = function(options, func) {
+exports.register = function (options, func) {
 	options.func = func;
 	calls.push(options);
-}
+};
 
 function parsePathParams(pattern, path) {
-	var a1 = pattern.split("/"), a2 = path.split("/");
-	var params = {}
-	if (a1.length != a2.length)
+	var a1 = pattern.split("/"), a2 = path.split("/"), params = {}, i;
+	if (a1.length !== a2.length) {
 		return {};
+	}
 	if (a2[a2.length - 1].indexOf(".") !== -1) {
 		params.output = a2[a2.length - 1].substring(a2[a2.length - 1].indexOf(".") + 1);
 		a2[a2.length - 1] = a2[a2.length - 1].substring(0, a2[a2.length - 1].indexOf("."));
 	}
-	for (var i = 0; i < a1.length; ++i) {
-
-		if (a1[i].charAt(0) == "{" && a1[i].charAt(a1[i].length - 1) == "}") {
+	for (i = 0; i < a1.length; i += 1) {
+		if (a1[i].charAt(0) === "{" && a1[i].charAt(a1[i].length - 1) === "}") {
 			params[a1[i].substring(1, a1[i].length - 1)] = a2[i];
 		}
 	}
@@ -59,12 +64,13 @@ function parsePathParams(pattern, path) {
 }
 
 function pathMatch(pattern, path) {
-	var a1 = pattern.split("/"), a2 = path.split("/");
-	if (a1.length != a2.length)
+	var a1 = pattern.split("/"), a2 = path.split("/"), i;
+	if (a1.length !== a2.length) {
 		return false;
-	for (var i = 0; i < a1.length; ++i) {
-		if (a1[i].charAt(0) != "{" && a1[i].charAt(a1[i].length - 1) != "}" && a1[i] != a2[i]) {
-			return false;			
+	}
+	for (i = 0; i < a1.length; i += 1) {
+		if (a1[i].charAt(0) !== "{" && a1[i].charAt(a1[i].length - 1) !== "}" && a1[i] !== a2[i]) {
+			return false;
 		}
 	}
 	return true;
@@ -72,15 +78,17 @@ function pathMatch(pattern, path) {
 
 function badRequest(response, body) {
 	response.writeHead(400, "Bad Request");
-	if (body)
+	if (body) {
 		response.write(body);
+	}
 	response.end();
 }
 
 function unauthorized(response, body) {
 	response.writeHead(401, "Unauthorized");
-	if (body)
+	if (body) {
 		response.write(body);
+	}
 	response.end();
 }
 
@@ -92,13 +100,11 @@ function send_cors_headers(request, response) {
             response.setHeader("Access-Control-Allow-Methods", "PUT, DELETE");
             response.setHeader("Access-Control-Allow-Headers", "accept, access-control-allow-credentials, x-requested-with, origin, content-type");
 			response.writeHead(200, "OK");
-		}
-		else {
+		} else {
 			response.writeHead(400, "Bad Request");
 		}
 
-	}
-	else {
+	} else {
 		// add CORS headers
 		if (request.headers.origin) {
             response.setHeader("Access-Control-Allow-Origin", request.headers.origin);
@@ -107,31 +113,30 @@ function send_cors_headers(request, response) {
     }
 }
 
-exports.run =  function(port, hostname) {
+exports.run =  function (port, hostname) {
 	var server = http.createServer().listen(port, hostname);
-	server.on("request", function(request, response) {
+	server.on("request", function (request, response) {
 		var body = "";
-		request.on("data", function(chunk) {
+		request.on("data", function (chunk) {
 			body += chunk;
 		});
-		request.on("end", function() {
-			var path = require("url").parse(request.url, true).pathname;
-			for (var i = 0; i < calls.length; ++i) {
-				var call = calls[i];
+		request.on("end", function () {
+			var path = require("url").parse(request.url, true).pathname, i, call, token, params;
+			for (i = 0; i < calls.length; i += 1) {
+				call = calls[i];
 				if ((call.method === request.method || request.method === "OPTIONS") && pathMatch(call.path, path)) {
 					send_cors_headers(request, response);
 					if (request.method !== "OPTIONS") {
-						var token = getToken(request.headers);
-						if (call.needAuth && !users[token])
+						token = getToken(request.headers);
+						if (call.needAuth && !users[token]) {
 							unauthorized(response);
-						else {
-							var params = parseParams(request, body);
+						} else {
+							params = parseParams(request, body);
 							params.path = parsePathParams(call.path, path);
 							params.token = token;
 							call.func(params, response);
 						}
-					}
-					else {
+					} else {
 						response.end();
 					}
 					return;
@@ -140,4 +145,4 @@ exports.run =  function(port, hostname) {
 			badRequest(response);
 		});
 	});
-}
+};

src/back/server.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var api = require("./api-utils");
 var couchWrapper = require("./couch-wrapper");
 var crypto = require("crypto");
@@ -9,169 +11,151 @@ api.register({
 	method: "POST",
 	path: "/users/signin",
 	needAuth: false
-	}, 
-	function(params, response) {
-		var shasum = crypto.createHash("sha512").update(params.passwd, "utf8").digest("hex");
-		couchWrapper.userLogin(params.login, function(user_data) {
-			if (user_data && user_data.shasum && shasum == user_data.shasum) {
-				api.registerUser(user_data.uuid, params.login);
-				response.setHeader("Set-Cookie", "token="+user_data.uuid+"; path=/; expires=Wed, 14-Jan-2032 16:16:49 GMT");
-				response.writeHead(204, "No Content");
-			}
-			else {
-				response.writeHead(403, "Forbidden");
-			}
-			response.end();
-		});
-	}
-);
+}, function (params, response) {
+	var shasum = crypto.createHash("sha512").update(params.passwd, "utf8").digest("hex");
+	couchWrapper.userLogin(params.login, function (user_data) {
+		if (user_data && user_data.shasum && shasum === user_data.shasum) {
+			api.registerUser(user_data.uuid, params.login);
+			response.setHeader("Set-Cookie", "token=" + user_data.uuid + "; path=/; expires=Wed, 14-Jan-2032 16:16:49 GMT");
+			response.writeHead(204, "No Content");
+		} else {
+			response.writeHead(403, "Forbidden");
+		}
+		response.end();
+	});
+});
 
 api.register({
 	method: "POST",
 	path: "/user/signout",
 	needAuth: true
-	},
-	function(params, response) {
-		api.unregisterUser(params.token)
-		response.setHeader("Set-Cookie", "token=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT");
-		response.writeHead(204, "No Content");
-		response.end();
-	}
-);
+}, function (params, response) {
+	api.unregisterUser(params.token);
+	response.setHeader("Set-Cookie", "token=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT");
+	response.writeHead(204, "No Content");
+	response.end();
+});
+
 api.register({
 	method: "POST",
 	path: "/users/signup",
 	needAuth: false
-	},
-	function(params, response) {
-		var shasum = crypto.createHash("sha512").update(params.passwd, "utf8").digest("hex");
-		params.shasum = shasum;
-		// ... then delete the password
-		delete params.passwd;
-		couchWrapper.userCreate(params, function(uuid) {
-			if (uuid) {
-				api.registerUser(uuid, params.login);
-				response.setHeader("Set-Cookie", "token="+uuid+"; path=/; expires=Wed, 14-Jan-2032 16:16:49 GMT");
-				response.writeHead(201, "Created");
-			}
-			else {
-				response.writeHead(403, "Forbidden");
-			}
-			response.end();
-		});
-	}
-);
+}, function (params, response) {
+	var shasum = crypto.createHash("sha512").update(params.passwd, "utf8").digest("hex");
+	params.shasum = shasum;
+	// ... then delete the password
+	delete params.passwd;
+	couchWrapper.userCreate(params, function (uuid) {
+		if (uuid) {
+			api.registerUser(uuid, params.login);
+			response.setHeader("Set-Cookie", "token=" + uuid + "; path=/; expires=Wed, 14-Jan-2032 16:16:49 GMT");
+			response.writeHead(201, "Created");
+		} else {
+			response.writeHead(403, "Forbidden");
+		}
+		response.end();
+	});
+});
 
 api.register({
 	method: "GET",
 	path: "/user",
 	needAuth: true
-	},
-	function(params, response) {
-		couchWrapper.userGet(api.getLogin(params.token), function(user_object) {
-			if (user_object) {
-				couchWrapper.docByUser(api.getLogin(params.token), function(docs_list) {
-					if (docs_list !== null) {
-						delete user_object.shasum;
-						user_object.documents = docs_list;
-						response.writeHead(200, "OK");
-						response.write(JSON.stringify(user_object));
-					}
-					else
-						response.writeHead(403, "Forbidden");
-					response.end();
-				});
-			}
-			else {
-				response.writeHead(403, "Forbidden");
-				response.end();	
-			}			
-		});
-	}
-);
+}, function (params, response) {
+	couchWrapper.userGet(api.getLogin(params.token), function (user_object) {
+		if (user_object) {
+			couchWrapper.docByUser(api.getLogin(params.token), function (docs_list) {
+				if (docs_list !== null) {
+					delete user_object.shasum;
+					user_object.documents = docs_list;
+					response.writeHead(200, "OK");
+					response.write(JSON.stringify(user_object));
+				} else {
+					response.writeHead(403, "Forbidden");
+				}
+				response.end();
+			});
+		} else {
+			response.writeHead(403, "Forbidden");
+			response.end();
+		}
+	});
+});
 
 api.register({
 	method: "GET",
 	path: "/users/{login}",
 	needAuth: true
-	},
-	function(params, response) {
-		couchWrapper.userGet(params.path.login, function(user_object) {
-			if (user_object) {
-				couchWrapper.docByUser(params.path.login, function(docs_list) {
-					if (docs_list !== null) {
-						delete user_object.shasum;
-						user_object.documents = docs_list;
-						response.writeHead(200, "OK");
-						response.write(JSON.stringify(user_object));
-					}
-					else
-						response.writeHead(403, "Forbidden");
-					response.end();
-				});
-			}
-			else {
-				response.writeHead(403, "Forbidden");
-				response.end();	
-			}			
-		});
-	}
-);
+}, function (params, response) {
+	couchWrapper.userGet(params.path.login, function (user_object) {
+		if (user_object) {
+			couchWrapper.docByUser(params.path.login, function (docs_list) {
+				if (docs_list !== null) {
+					delete user_object.shasum;
+					user_object.documents = docs_list;
+					response.writeHead(200, "OK");
+					response.write(JSON.stringify(user_object));
+				} else {
+					response.writeHead(403, "Forbidden");
+				}
+				response.end();
+			});
+		} else {
+			response.writeHead(403, "Forbidden");
+			response.end();
+		}
+	});
+});
 
 api.register({
 	method: "DELETE",
 	path: "/user",
 	needAuth: true
-	},
-	function (params, response) {
-		couchWrapper.userDelete(api.getLogin(params.token), function(success) {
-			if (success)
-				response.writeHead(204, "No Content");
-			else
-				response.writeHead(403, "Forbidden");
-			response.end();
-		});
-		api.unregisterUser(params.token);
-	}
-);
+}, function (params, response) {
+	couchWrapper.userDelete(api.getLogin(params.token), function (success) {
+		if (success) {
+			response.writeHead(204, "No Content");
+		} else {
+			response.writeHead(403, "Forbidden");
+		}
+		response.end();
+	});
+	api.unregisterUser(params.token);
+});
 
 api.register({
 	method: "GET",
 	path: "/users",
 	needAuth: true
-	},
-	function(params, response) {
-		var prefix = params.prefix ? params.prefix : "";
-		couchWrapper.userByPrefix(prefix, function(users) {
-			if (users !== null) {
-				if (users.length == 0) {
-					response.writeHead(204, "No Content");
-				}
-				else {
-					response.writeHead(200, "OK");
-					response.write(JSON.stringify(users));
-				}
+}, function (params, response) {
+	var prefix = params.prefix || "";
+	couchWrapper.userByPrefix(prefix, function (users) {
+		if (users !== null) {
+			if (users.length === 0) {
+				response.writeHead(204, "No Content");
+			} else {
+				response.writeHead(200, "OK");
+				response.write(JSON.stringify(users));
 			}
-			else {
-				response.writeHead(403, "Forbidden");
-			}
-			response.end();
-		});
-	}
-);
+		} else {
+			response.writeHead(403, "Forbidden");
+		}
+		response.end();
+	});
+});
 
 api.register({
 	method: "POST",
 	path: "/documents",
 	needAuth: true
 }, function (params, response) {
-	couchWrapper.docAdd(params, function(id) {
+	couchWrapper.docAdd(params, function (id) {
 		if (id) {
 			response.writeHead(201, "Created");
 			response.write(JSON.stringify(id));
-		}
-		else
+		} else {
 			response.writeHead(403, "Forbidden");
+		}
 		response.end();
 	});
 });
@@ -180,12 +164,13 @@ api.register({
 	method: "PUT",
 	path: "/documents/{id}",
 	needAuth: true
-}, function(params, response) {
-	couchWrapper.docUpdate(params, function(success) {
-		if (success)
+}, function (params, response) {
+	couchWrapper.docUpdate(params, function (success) {
+		if (success) {
 			response.writeHead(204, "No Content");
-		else
+		} else {
 			response.writeHead(403, "Forbidden");
+		}
 		response.end();
 	});
 });
@@ -194,12 +179,13 @@ api.register({
 	method: "DELETE",
 	path: "/documents/{id}",
 	needAuth: true
-}, function(params, response) {
-	couchWrapper.docDelete(params.path.id, function(success) {
-		if (success)
+}, function (params, response) {
+	couchWrapper.docDelete(params.path.id, function (success) {
+		if (success) {
 			response.writeHead(204, "No Content");
-		else
+		} else {
 			response.writeHead(403, "Forbidden");
+		}
 		response.end();
 	});
 });
@@ -208,8 +194,8 @@ api.register({
 	method: "GET",
 	path: "/documents/{id}/versions",
 	needAuth: true
-}, function(params, response) {
-	couchWrapper.getLastVersion(params.path.id, function(lastVersion) {
+}, function (params, response) {
+	couchWrapper.getLastVersion(params.path.id, function (lastVersion) {
 		response.writeHead(200, "OK");
 		response.write(JSON.stringify({lastVersion: lastVersion}));
 		response.end();
@@ -217,13 +203,12 @@ api.register({
 });
 
 function format_doc(doc) {
-	doc_md = {};
+	var doc_md = {}, md = "", i;
 	doc_md._id = doc._id;
 	doc_md.settings = require("../md2pdf/default.json");
-	var md = "";
-	md += "#"+doc.title+"\n";
-	for (var i = 0; i < doc.content.length; ++i) {
-		md += (new Array(doc.content[i].level + 2)).join("#") + " " + doc.content[i].title;
+	md += "#" + doc.title + "\n";
+	for (i = 0; i < doc.content.length; i += 16) {
+		md += [doc.content[i].level + 2].join("#") + " " + doc.content[i].title;
 		md += "\n";
 		md += doc.content[i].content;
 		md += "\n";
@@ -236,30 +221,28 @@ api.register({
 	method: "GET",
 	path: "/documents/{id}",
 	needAuth: true
-}, function(params, response) {
-	couchWrapper.docGet(params.path.id, -1, function(doc) {
+}, function (params, response) {
+	couchWrapper.docGet(params.path.id, -1, function (doc) {
 		if (doc !== null) {
 			if (params.path.output) {
-				doc_md = format_doc(doc);
-				var req = http.request({hostname: "localhost", port: 8081, path: "/" + params.path.output, method: "POST"}, function(res) {
+				var doc_md = format_doc(doc), req;
+				req = http.request({hostname: "localhost", port: 8081, path: "/" + params.path.output, method: "POST"}, function (res) {
 					response.writeHead(200, "OK");
-					res.on("data", function(buffer) {
+					res.on("data", function (buffer) {
 						response.write(buffer);
 					});
-					res.on("end", function() {
+					res.on("end", function () {
 						response.end();
 					});
 				});
 				req.write(JSON.stringify(doc_md));
 				req.end();
-			}
-			else {
+			} else {
 				response.writeHead(200, "OK");
 				response.write(JSON.stringify(doc));
 				response.end();
-			}				
-		}
-		else {
+			}
+		} else {
 			response.writeHead(403, "Forbidden");
 			response.end();
 		}
@@ -270,30 +253,28 @@ api.register({
 	method: "GET",
 	path: "/documents/{id}/versions/{version}",
 	needAuth: true
-}, function(params, response) {
-	couchWrapper.docGet(params.path.id, params.path.version, function(doc) {
+}, function (params, response) {
+	couchWrapper.docGet(params.path.id, params.path.version, function (doc) {
 		if (doc !== null) {
 			if (params.path.output) {
-				doc_md = format_doc(doc);
-				var req = http.request({hostname: "localhost", port: 8081, path: "/" + params.path.output, method: "POST"}, function(res) {
+				var doc_md = format_doc(doc), req;
+				req = http.request({hostname: "localhost", port: 8081, path: "/" + params.path.output, method: "POST"}, function (res) {
 					response.writeHead(200, "OK");
-					res.on("data", function(buffer) {
+					res.on("data", function (buffer) {
 						response.write(buffer);
 					});
-					res.on("end", function() {
+					res.on("end", function () {
 						response.end();
 					});
 				});
 				req.write(JSON.stringify(doc_md));
 				req.end();
-			}
-			else {
+			} else {
 				response.writeHead(200, "OK");
 				response.write(JSON.stringify(doc));
 				response.end();
-			}				
-		}
-		else {
+			}
+		} else {
 			response.writeHead(403, "Forbidden");
 			response.end();
 		}
@@ -301,5 +282,4 @@ api.register({
 });
 
 /* start the server */
-
 api.run(8080);

commit 4599e0b3d87029558c71b82ce667989ab08eedee

Fix regression introduced by commit e4155fb7e9.

src/back/api-utils.js
@@ -5,8 +5,6 @@ var http = require("http");
 var users = [];
 users.token = "kikoo";
 
-var exports = {};
-
 exports.registerUser = function (token, login) {
 	users[token] = login;
 };