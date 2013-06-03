967f226e351d8851cacd507b61b3d4a3cc5050cc
Baptiste Metge <baptiste.metge@insa-lyon.fr>
2013-06-03 16:31:48 +0200

    Notifications

--- src/front/app/scripts/controllers/top-bar.js
+++ src/front/app/scripts/controllers/top-bar.js
@@ -2,6 +2,8 @@
 
 angular.module('pie')
 .controller('TopBarController', function ($scope, authService) {
+	authService.ensureLogin();
+	
 	$scope.isSectionActive = function(section) {
 		return ($scope.currentPage == section) ? 'active' : '';
 	};
@@ -11,15 +13,11 @@ angular.module('pie')
 		function(newVal) {
 			if (! authService.user) return;			
 			$scope.user = newVal;
-			$scope.login = authService.user.login;
-			$scope.numberOfNotification = authService.user.notifications.length;
-			$scope.notifications = authService.user.notifications;
-			var i =0 ;
-			for ( i =0 ; i< $scope.numberOfNotification ; i++ ) {
-				if ( $scope.notifications[i].type === "document" ) {
-					$scope.notifications[i].path = "/#/editAndDiscuss/"+$scope.notifications[i].id;
-				} else if ($scope.notifications[i].type === "discussion" ){
-					$scope.notifications[i].path = "/#/editAndDiscuss/"+$scope.notifications[i].id+"";
+			for (var i =0 ; i< $scope.user.notifications.length ; i++ ) {
+				if ( $scope.user.notifications[i].type === "document" ) {
+					$scope.user.notifications[i].path = "/#/editAndDiscuss/"+$scope.user.notifications[i].id;
+				} else if ($scope.user.notifications[i].type === "discussion" ){
+					$scope.user.notifications[i].path = "/#/editAndDiscuss/"+$scope.user.notifications[i].id+"";
 				}
 			}
 		}
