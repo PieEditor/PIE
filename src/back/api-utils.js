b6123b9339d76b1120486fb081a2be72b3252e23
Guillaume Burel <guillaume@yimgo.fr>
2013-06-03 09:31:04 +0200

    Improved server code quality.
    Using JSLint.

--- src/back/api-utils.js
+++ src/back/api-utils.js
@@ -1,57 +1,62 @@
+"use strict";
+
 var http = require("http");
 
 var users = [];
-users["token"] = "kikoo";
+users.token = "kikoo";
 
-exports.registerUser = function(token, login) {
+var exports = {};
+
+exports.registerUser = function (token, login) {
 	users[token] = login;
-}
+};
 
-exports.unregisterUser = function(token) {
+exports.unregisterUser = function (token) {
 	delete users[token];
-}
+};
 
-exports.getLogin = function(token) {
+exports.getLogin = function (token) {
 	return users[token];
-}
+};
 
-function parseParams(request, body) {				
+function parseParams(request, body) {
 	var params = {};
-	if ((request.method === "POST" || request.method === "PUT" || request.method === "DELETE") && body)
+	if ((request.method === "POST" || request.method === "PUT" || request.method === "DELETE") && body) {
 		params = JSON.parse(body);
-	else if (request.method === "GET")
+	} else if (request.method === "GET") {
 		params = require("url").parse(request.url, true).query;
+	}
 	return params;
 }
 
 function getToken(headers) {
-	// parse cookies in order to get the access token - thanks to Corey Hart & ianj : http://stackoverflow.com/questions/3393854/get-and-set-a-single-cookie-with-node-js-http-server
 	var cookies = {};
-	headers.cookie && headers.cookie.split(";").forEach(function(cookie) {
-		var parts = cookie.split("=");
-		cookies[parts[0].trim()] = (parts[1] || "").trim();
-	});
-	return cookies["token"];
+	if (headers.cookie) {
+		headers.cookie.split(";").forEach(function (cookie) {
+			var parts = cookie.split("=");
+			cookies[parts[0].trim()] = (parts[1] || "").trim();
+		});
+	}
+	return cookies.token;
 }
 
 var calls = [];
-exports.register = function(options, func) {
+exports.register = function (options, func) {
 	options.func = func;
 	calls.push(options);
-}
+};
 
 function parsePathParams(pattern, path) {
-	var a1 = pattern.split("/"), a2 = path.split("/");
-	var params = {}
-	if (a1.length != a2.length)
+	var a1 = pattern.split("/"), a2 = path.split("/"), params = {}, i;
+	if (a1.length !== a2.length) {
 		return {};
+	}
 	if (a2[a2.length - 1].indexOf(".") !== -1) {
 		params.output = a2[a2.length - 1].substring(a2[a2.length - 1].indexOf(".") + 1);
 		a2[a2.length - 1] = a2[a2.length - 1].substring(0, a2[a2.length - 1].indexOf("."));
 	}
-	for (var i = 0; i < a1.length; ++i) {
-
-		if (a1[i].charAt(0) == "{" && a1[i].charAt(a1[i].length - 1) == "}") {
+	for (i = 0; i < a1.length; i += 1) {
+		if (a1[i].charAt(0) === "{" && a1[i].charAt(a1[i].length - 1) === "}") {
 			params[a1[i].substring(1, a1[i].length - 1)] = a2[i];
 		}
 	}
@@ -59,12 +64,13 @@ function parsePathParams(pattern, path) {
 }
 
 function pathMatch(pattern, path) {
-	var a1 = pattern.split("/"), a2 = path.split("/");
-	if (a1.length != a2.length)
+	var a1 = pattern.split("/"), a2 = path.split("/"), i;
+	if (a1.length !== a2.length) {
 		return false;
-	for (var i = 0; i < a1.length; ++i) {
-		if (a1[i].charAt(0) != "{" && a1[i].charAt(a1[i].length - 1) != "}" && a1[i] != a2[i]) {
-			return false;			
+	}
+	for (i = 0; i < a1.length; i += 1) {
+		if (a1[i].charAt(0) !== "{" && a1[i].charAt(a1[i].length - 1) !== "}" && a1[i] !== a2[i]) {
+			return false;
 		}
 	}
 	return true;
@@ -72,15 +78,17 @@ function pathMatch(pattern, path) {
 
 function badRequest(response, body) {
 	response.writeHead(400, "Bad Request");
-	if (body)
+	if (body) {
 		response.write(body);
+	}
 	response.end();
 }
 
 function unauthorized(response, body) {
 	response.writeHead(401, "Unauthorized");
-	if (body)
+	if (body) {
 		response.write(body);
+	}
 	response.end();
 }
 
@@ -92,13 +100,11 @@ function send_cors_headers(request, response) {
             response.setHeader("Access-Control-Allow-Methods", "PUT, DELETE");
             response.setHeader("Access-Control-Allow-Headers", "accept, access-control-allow-credentials, x-requested-with, origin, content-type");
 			response.writeHead(200, "OK");
-		}
-		else {
+		} else {
 			response.writeHead(400, "Bad Request");
 		}
 
-	}
-	else {
+	} else {
 		// add CORS headers
 		if (request.headers.origin) {
             response.setHeader("Access-Control-Allow-Origin", request.headers.origin);
@@ -107,31 +113,30 @@ function send_cors_headers(request, response) {
     }
 }
 
-exports.run =  function(port, hostname) {
+exports.run =  function (port, hostname) {
 	var server = http.createServer().listen(port, hostname);
-	server.on("request", function(request, response) {
+	server.on("request", function (request, response) {
 		var body = "";
-		request.on("data", function(chunk) {
+		request.on("data", function (chunk) {
 			body += chunk;
 		});
-		request.on("end", function() {
-			var path = require("url").parse(request.url, true).pathname;
-			for (var i = 0; i < calls.length; ++i) {
-				var call = calls[i];
+		request.on("end", function () {
+			var path = require("url").parse(request.url, true).pathname, i, call, token, params;
+			for (i = 0; i < calls.length; i += 1) {
+				call = calls[i];
 				if ((call.method === request.method || request.method === "OPTIONS") && pathMatch(call.path, path)) {
 					send_cors_headers(request, response);
 					if (request.method !== "OPTIONS") {
-						var token = getToken(request.headers);
-						if (call.needAuth && !users[token])
+						token = getToken(request.headers);
+						if (call.needAuth && !users[token]) {
 							unauthorized(response);
-						else {
-							var params = parseParams(request, body);
+						} else {
+							params = parseParams(request, body);
 							params.path = parsePathParams(call.path, path);
 							params.token = token;
 							call.func(params, response);
 						}
-					}
-					else {
+					} else {
 						response.end();
 					}
 					return;
@@ -140,4 +145,4 @@ exports.run =  function(port, hostname) {
 			badRequest(response);
 		});
 	});
-}
+};
