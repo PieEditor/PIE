07c5d7f7fbafb065a22bf2e3788aaef1e01b12de
Guillaume Burel <guillaume@yimgo.fr>
2013-06-05 08:28:59 +0200

    Send the entire discussion in cas of new discussion or discussion answer through socket.io.

--- src/back/notifications.js
+++ src/back/notifications.js
@@ -68,15 +68,15 @@ exports.notificationsOfChange = function(old_doc, new_doc, login) {
 				continue;
 			}
 			if ((!old_doc.content[i].discussions && new_doc.content[i].discussions) || (old_doc.content[i].discussions.length < new_doc.content[i].discussions.length)) {
-				notifications.push({type: "discussion", text: login + " started a new discussion about section \"" + new_doc.content[i].title + "\" of \"" + old_doc.title + "\".", docId: new_doc.docId, sectionIndex: i, discussionIndex: new_doc.content[i].discussions.length});
+				notifications.push({type: "discussion", text: login + " started a new discussion about section \"" + new_doc.content[i].title + "\" of \"" + old_doc.title + "\".", docId: new_doc.docId, sectionIndex: i, discussionIndex: new_doc.content[i].discussions.length - 1, discussion: new_doc.content[i].discussions[new_doc.content[i].discussions.length - 1]});
 			}
 			else if (old_doc.content[i].discussions.length === new_doc.content[i].discussions.length) {
 				for (j = 0; j < old_doc.content[i].discussions.length; j += 1) {
 					if (new_doc.content[i].discussions[j].resolved && !old_doc.content[i].discussions[j].resolved) {
 						notifications.push({type: "discussion", text: login + " resolved the discussion \"" + old_doc.content[i].discussions[j].title + "\" which was about \"" + old_doc.content[i].title + "\" of \"" + old_doc.title + "\".", docId: new_doc.docId, sectionIndex: i, discussionIndex: j});
 					}
-					if (new_doc.content[i].discussions[j].posts.length !== old_doc.content[i].discussions[j].posts.length) {
-						notifications.push({type: "discussion", text: login + " answered the discussion \"" + old_doc.content[i].discussions[j].title + "\" which was about \"" + old_doc.content[i].title + "\" of \"" + old_doc.title + "\".", docId: new_doc.docId, sectionIndex: i, discussionIndex: j, post: new_doc.content[i].discussions[j].posts[new_doc.content[i].discussions[j].posts.length - 1]});
+					if (new_doc.content[i].discussions[j].posts.length > old_doc.content[i].discussions[j].posts.length) {
+						notifications.push({type: "discussion", text: login + " answered the discussion \"" + old_doc.content[i].discussions[j].title + "\" which was about \"" + old_doc.content[i].title + "\" of \"" + old_doc.title + "\".", docId: new_doc.docId, sectionIndex: i, discussionIndex: j, discussion: new_doc.content[i].discussions[j]});
 					}
 				}
 			}
