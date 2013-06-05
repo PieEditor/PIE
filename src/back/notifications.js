46c995e69120847ea9772fe7d9e3fd37c457f1f4
Guillaume Burel <guillaume@yimgo.fr>
2013-06-05 11:36:46 +0200

    Send a notification to new collaborators when adding such.
    Fix issue #58.

--- src/back/notifications.js
+++ src/back/notifications.js
@@ -49,10 +49,34 @@ exports.notify = function(login, notification) {
 
 exports.notificationsOfCreation = function(doc) {
 	var notifications = [];
-	notifications.push({type: "document", text: doc.owner + " added you to the collaborators list of \"" + doc.title + "\".", docId: doc.docId});
-	console.log(JSON.stringify(notifications));
+	notifications.push(
+		{
+			notifieds: exports.notifieds(doc.owner, exports.collaboratorsLogins(doc.collaborators), doc.owner),
+			notification: {type: "document", text: doc.owner + " added you to the collaborators list of \"" + doc.title + "\".", docId: doc.docId}
+		}
+	);
 	return notifications;
 };
+
+function collaboratorsChanged(a, b) {
+	var new_coll = [];
+    if (!a || !b) {
+    	if (!a && !b) {
+    		return [];
+    	} else if (!a & b) {
+    		return b;
+    	} else {
+    		return [];
+    	}
+    }
+
+    for (var i = 0; i < b.length; i += 1) {
+    	if (a.indexOf(b[i]) === -1) {
+    		new_coll.push(b[i]);
+    	}
+    }
+    return new_coll;
+}
 
 exports.notificationsOfChange = function(old_doc, new_doc, login) {
 	var notifications = [], i, j;
@@ -60,24 +84,50 @@ exports.notificationsOfChange = function(old_doc, new_doc, login) {
 		return [];
 	}
 	if (new_doc.content.length !== old_doc.content.length) {
-		notifications.push({type: "document", text: login + " changed the architecture of \"" + old_doc.title + "\".", docId: new_doc.docId});
+		notifications.push(
+			{
+				notifieds: exports.notifieds(new_doc.owner, exports.collaboratorsLogins(new_doc.collaborators), login),
+				notification: {type: "document", text: login + " changed the architecture of \"" + old_doc.title + "\".", docId: new_doc.docId}
+			}
+		);
 	}
-	else {
-		for (i = 0; i < old_doc.content.length; i += 1) {
-			if (!old_doc.content[i].discussions && !new_doc.content[i].discussions) {
-				continue;
+	if (collaboratorsChanged(exports.collaboratorsLogins(old_doc.collaborators), exports.collaboratorsLogins(new_doc.collaborators)).length > 0) {
+		notifications.push(
+			{
+				notifieds: collaboratorsChanged(exports.collaboratorsLogins(old_doc.collaborators), exports.collaboratorsLogins(new_doc.collaborators)),
+				notification: {type: "document", text: login + " added you to the collaborators list of \"" + new_doc.title + "\".", docId: new_doc.docId}
 			}
-			if ((!old_doc.content[i].discussions && new_doc.content[i].discussions) || (old_doc.content[i].discussions.length < new_doc.content[i].discussions.length)) {
-				notifications.push({type: "discussion", text: login + " started a new discussion about section \"" + new_doc.content[i].title + "\" of \"" + old_doc.title + "\".", docId: new_doc.docId, sectionIndex: i, discussionIndex: new_doc.content[i].discussions.length - 1, discussion: new_doc.content[i].discussions[new_doc.content[i].discussions.length - 1]});
-			}
-			else if (old_doc.content[i].discussions.length === new_doc.content[i].discussions.length) {
-				for (j = 0; j < old_doc.content[i].discussions.length; j += 1) {
-					if (new_doc.content[i].discussions[j].resolved && !old_doc.content[i].discussions[j].resolved) {
-						notifications.push({type: "discussion", text: login + " resolved the discussion \"" + old_doc.content[i].discussions[j].title + "\" which was about \"" + old_doc.content[i].title + "\" of \"" + old_doc.title + "\".", docId: new_doc.docId, sectionIndex: i, discussionIndex: j});
-					}
-					if (new_doc.content[i].discussions[j].posts.length > old_doc.content[i].discussions[j].posts.length) {
-						notifications.push({type: "discussion", text: login + " answered the discussion \"" + old_doc.content[i].discussions[j].title + "\" which was about \"" + old_doc.content[i].title + "\" of \"" + old_doc.title + "\".", docId: new_doc.docId, sectionIndex: i, discussionIndex: j, discussion: new_doc.content[i].discussions[j]});
-					}
+		);
+	}
+	for (i = 0; i < old_doc.content.length; i += 1) {
+		if (!old_doc.content[i].discussions && !new_doc.content[i].discussions) {
+			continue;
+		}
+		if ((!old_doc.content[i].discussions && new_doc.content[i].discussions) || (old_doc.content[i].discussions.length < new_doc.content[i].discussions.length)) {
+			notifications.push(
+				{
+					notifieds: exports.notifieds(new_doc.owner, exports.collaboratorsLogins(new_doc.collaborators), login),
+					notification: {type: "discussion", text: login + " started a new discussion about section \"" + new_doc.content[i].title + "\" of \"" + old_doc.title + "\".", docId: new_doc.docId, sectionIndex: i, discussionIndex: new_doc.content[i].discussions.length - 1, discussion: new_doc.content[i].discussions[new_doc.content[i].discussions.length - 1]}
+				}
+			);
+		}
+		else if (old_doc.content[i].discussions.length === new_doc.content[i].discussions.length) {
+			for (j = 0; j < old_doc.content[i].discussions.length; j += 1) {
+				if (new_doc.content[i].discussions[j].resolved && !old_doc.content[i].discussions[j].resolved) {
+					notifications.push(
+						{
+							notifieds: exports.notifieds(new_doc.owner, exports.collaboratorsLogins(new_doc.collaborators), login),
+							notification: {type: "discussion", text: login + " resolved the discussion \"" + old_doc.content[i].discussions[j].title + "\" which was about \"" + old_doc.content[i].title + "\" of \"" + old_doc.title + "\".", docId: new_doc.docId, sectionIndex: i, discussionIndex: j}
+						}
+					);
+				}
+				if (new_doc.content[i].discussions[j].posts.length > old_doc.content[i].discussions[j].posts.length) {
+					notifications.push(
+						{
+							notifieds: exports.notifieds(new_doc.owner, exports.collaboratorsLogins(new_doc.collaborators), login),
+							notification: {type: "discussion", text: login + " answered the discussion \"" + old_doc.content[i].discussions[j].title + "\" which was about \"" + old_doc.content[i].title + "\" of \"" + old_doc.title + "\".", docId: new_doc.docId, sectionIndex: i, discussionIndex: j, discussion: new_doc.content[i].discussions[j]}
+						}
+					);
 				}
 			}
 		}
