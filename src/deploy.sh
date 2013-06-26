366154f3e1f99c4539dcd043c1ca080d7a4d7674
Guillaume Burel <guillaume@yimgo.fr>
2013-06-26 15:12:56 +0200

    Fix sript invocation comment.
    Add she-bang too.

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 # ./deploy.sh [host]
 # eg.
 # To deploy to the default server
 # $ ./deploy.sh
 # To deploy to another server:
-# $ ./deploy.fr another.server.com
+# $ ./deploy.sh another.server.com
 
 host=${1:-yimgo.fr}
 ssh pie@$host "cd devel/Communities; git pull; make -C src/md2pdf; cd src/front; killall node; npm install ; bower install ; grunt build ; cd ../back ; node init-db.js ; node update-db.js ; (nohup node server.js > server.js.out 2> server.js.err &) ; cd ../md2pdf ; (nohup node convert.js > convert.js.out 2> convert.js.out &)"
